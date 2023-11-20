5de4a1c2dcff3304b6274e48043a5f2855c54400
AryanJindal <jindalaryan070707@gmail.com>
2023-11-20 10:49:35 +0530

    First video for the basics of react

---
+++ lec1part3/App.js
@@ -0,0 +1,34 @@
+// const heading = React.createElement("h1", {id : "heading"}, "Hello world from react")
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading); 
+
+{/* <div id = "parent">
+    <div id="child1">
+        <h1> I am a h1 tag</h1>
+        <h2> I am a h2 tag</h2>
+    </div>
+    <div id="child2">
+        <h1> I am a h1 tag</h1>
+        <h2> I am a h2 tag</h2>
+    </div>
+</div> */}
+
+
+
+const parent = React.createElement("div", {id: "parent"}, [
+    React.createElement("div",{id : "child1"}, [
+        React.createElement("h1", {}, "I am a h1 element"),
+        React.createElement("h2", {}, "I am a h2 element")
+    ]),
+    React.createElement("div",{id : "child1"}, [
+        React.createElement("h1", {}, "I am a h1 element"),
+        React.createElement("h2", {}, "I am a h2 element")
+    ])
+])
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent); 
