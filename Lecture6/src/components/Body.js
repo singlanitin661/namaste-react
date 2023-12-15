0f09fcc0b922e8b3b55a701d1228c2a91bfec312
AryanJindal <jindalaryan070707@gmail.com>
2023-12-15 13:48:44 +0530

    Fixed infinite  fetch call in useEfeect due to missing []

--- Lecture6/src/components/Body.js
+++ Lecture6/src/components/Body.js
@@ -6,40 +6,58 @@ const styleCard = {
 const resList = []
 const Swiggy_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
 
+const CDN_URL =
+  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 const RestaurantComponent = (props) => {
+    // console.log(props)
+    const { resData } = props;
+
+    const {
+        cloudinaryImageId,
+        name,
+        avgRating,
+        cuisines,
+        costForTwo,
+        deliveryTime,
+    } = resData;
+    console.log(cuisines);
+    console.log(typeof cuisines);
+
     return (
         <div className="res-card" style={styleCard}>
-            <img src={props.image.search("example") == -1 ? props.image : "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/obtyqt35sq55t0owwixq"} alt={props.name + " Image"} ></img>
-            <h3>{props.name}</h3>
-            <h4>{props.cusine}</h4>
-            <h4>{props.rating + "/5⭐"} </h4>
-            <h4>{props.eta + " mins away"}</h4>
+            <img src={CDN_URL + cloudinaryImageId} style={{"border-radius" :"10px"}} ></img>
+            <h3>{name}</h3>
+            <h4>{cuisines.join(', ')}</h4>
+            <h4>{avgRating + "/5⭐"} </h4>
+            <h4>{deliveryTime} </h4>
         </div>
     )
 }
 
+
 const BodyComponent = () => {
 
-    const [listOfRestaurants, setListofRestaurants] = useState(resList);
+    const [listOfRestaurants, setListofRestaurants] = useState([]);
     const fetchData = async () =>{
         const data = await fetch(Swiggy_API);
         const dataInJSON = await data.json();
-        console.log(dataInJSON);
-        // setListofRestaurants(dataInJSON.data.cards[2].data.data.cards)
+        // console.log( dataInJSON.data.cards[5].card.card.gridElements.infoWithStyle.restaurants.length);
+        console.log("Hey")
+        setListofRestaurants(dataInJSON.data.cards[5].card.card.gridElements.infoWithStyle.restaurants)
     }
-
+    
+    
     useEffect(()=>{
         fetchData();
-    })
+    }, [])
     if(listOfRestaurants.length === 0){
         return <ShimmerComponent/>
     }
+    // return <h1>HJello</h1>
     return (
         <div className="body">
-            {/* <div className="search">
-                Search
-            </div> */}
+            
             <div className="filter">
                 <button className="filter-btn" onClick={()=>{
                     console.log("button clicked")
@@ -47,10 +65,19 @@ const BodyComponent = () => {
                 >Top Rated Rest</button>
             </div>
             <div className="restaurant-container">
-                {/* listOfRestaurants.map((rest) => (<RestaurantComponent key={restaurant.data.id} resData = {restaurant}/>)) */}
+               {
+                listOfRestaurants.map((restaurant) => 
+                    
+                   
+                        <RestaurantComponent key={restaurant.info.id} resData={restaurant.info} />
+                    
+                )
+               }                
             </div>
         </div>
     )
 }
+
+
 
 export default BodyComponent
