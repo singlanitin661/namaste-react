aebbf9dd0f064b61cb294a81543680e5a2830b85
singlanitin661 <singlanitin661@gmail.com>
2024-01-19 18:34:08 +0530

    l2 update

--- Lecture1/lec1part3/App.js
+++ Lecture1/lec1part3/App.js
@@ -1,34 +1,8 @@
-// const heading = React.createElement("h1", {id : "heading"}, "Hello world from react")
-// const root = ReactDOM.createRoot(document.getElementById("root"));
+// const root = document.getElementById("root") ;
+// const heading = document.createElement("h1") ;
+// heading.innerHTML = "Hello from JS" ;
+// root.appendChild(heading) ;
 
-// root.render(heading); 
-
-{/* <div id = "parent">
-    <div id="child1">
-        <h1> I am a h1 tag</h1>
-        <h2> I am a h2 tag</h2>
-    </div>
-    <div id="child2">
-        <h1> I am a h1 tag</h1>
-        <h2> I am a h2 tag</h2>
-    </div>
-</div> */}
-
-
-
-const parent = React.createElement("div", {id: "parent"}, [
-    React.createElement("div",{id : "child1"}, [
-        React.createElement("h1", {}, "I am a h1 element"),
-        React.createElement("h2", {}, "I am a h2 element")
-    ]),
-    React.createElement("div",{id : "child1"}, [
-        React.createElement("h1", {}, "I am a h1 element"),
-        React.createElement("h2", {}, "I am a h2 element")
-    ])
-])
-
-console.log(parent);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent); 
+const root = ReactDOM.createRoot(document.getElementById("root")) ;
+const heading = React.createElement("h1" , {} , "Hello from React!") ;
+root.render(heading) ;
---
+++ Lecture1/lec1part3/readme.md
@@ -0,0 +1,5 @@
+The costliest thing in dom is dom manupulation
+```{}``` : The empty {} this was used to give attributes. 
+Currently we have created a heading with no attribute , If we want to give
+an id to this heading 
+```{ id : "heading"}```
